4eddd88daad10c4ea0ac9737fcabe67b8577952e
Michael Kurowski <Byamarro@gmail.com>
2017-05-09 00:12:08 +0200

    [server][index] Users routers added

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cfg = require('./config')
 const port = cfg.getWebServerPort()
+
 const todosRouter = require('./routers/todos.js')
+const usersRouter = require('./routers/users.js')
 const seedDbRouter = require('./routers/seedDb.js')
+
+
 
 app.use('/assets', express.static(`${__dirname}/public`))
 app.use(bodyParser.json())
 app.set('view-engine', 'ejs')
+
 app.use('/api/setupTodos', seedDbRouter)
 app.use('/api/todos', todosRouter)
-mongoose.connect(cfg.getDbConnectionString())
+app.use('/api/users', usersRouter)
+mongoose.connect(cfg.getDbConnectionString()).catch(err => console.log(err))
 app.listen(port)
