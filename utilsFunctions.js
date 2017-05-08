6d448459177db28d5ec438e7280ad3d5b72ef2a0
Michael Kurowski <Byamarro@gmail.com>
2017-05-09 00:09:59 +0200

    [server][utilsFunctions] Reduced side-effects

--- utilsFunctions.js
+++ utilsFunctions.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 module.exports = {
 	throwErrorLog(endpoint, err) {
-		res.send('An error occured. Check server logs for details.')
 		fs.writeFile('logs.txt', `[${new Date()}] /${endpoint} - ${err}`, err => {
 			console.log(`[${new Date()}] /${endpoint} - ${err}`)
 		})
