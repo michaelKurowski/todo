a19d21207128c234235fe3d0249194623c5a186c
Michael Kurowski <Byamarro@gmail.com>
2017-05-28 15:24:04 +0200

    Gulp (babel transpilling)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 
-gulp.task('legacyBrowsers', function () {
-  return gulp.src('public/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('oldBrowsers/'))
+gulp.task('babel', function () {
+  return gulp.src('publicDev/*.js')
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest('public/'))
 })
+gulp.task('html', function () {
+  return gulp.src('publicDev/*.html')
+    .pipe(gulp.dest('public/'))
+})
+gulp.task('css', function () {
+  return gulp.src('publicDev/*.css')
+    .pipe(gulp.dest('public/'))
+})
+
+gulp.task('watch:css', () => gulp.watch('publicDev/*.css', ['css']))
+gulp.task('watch:js', () => gulp.watch('publicDev/*.js', ['babel']))
+gulp.task('watch:html', () => gulp.watch('publicDev/*.html', ['html']))
+
+gulp.task('default', () => gulp.start('watch:css', 'watch:js', 'watch:html'))
